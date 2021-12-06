15c84302d8cd826d6d0b7bd139310d384785e87d
newtykins <newtykins@outlook.com>
2021-12-06 21:33:18 +0000

    Add the search form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
 import Avatar from '../components/Avatar'
+import { MicrophoneIcon, ViewGridIcon } from '@heroicons/react/solid'
+import { SearchIcon } from '@heroicons/react/outline'
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Googol!</title>
-        <link rel="icon" href="/favicon.svg" />
+        <title>Google 2</title>
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <header className="flex w-full p-5 justify-between text-sm text-gray-700">
@@ -21,11 +24,29 @@ const Home: NextPage = () => {
           <p className="link">Gmail</p>
           <p className="link">Images</p>
 
-          {/* todo: Icon */}
+          <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
 
           <Avatar url="https://github.com/newtykins.png" />
         </div>
       </header>
+      
+      <form className="flex flex-col items-center mt-44 flex-grow">
+        {/* Google Icon */}
+        <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/368px-Google_2015_logo.svg.png" height={100} width={300} />
+
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+
+          <input type="text" className="focus:outline-none flex-grow" />
+
+          <MicrophoneIcon className="h-5" />
+        </div>
+
+        <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+          <button className="btn">Google Search</button>
+          <button className="btn">I'm Feeling Lucky</button>
+        </div>
+      </form>
     </div>
   )
 }
